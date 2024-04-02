962a94f587c58d24ad8f148dab9126d3d9bf170b
gonzalo <gonzalo.dev@gmail.com>
2024-04-02 19:25:36 -0300

    ticket pipeline

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar el servidor en el puerto 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
